09b18eec3f8668a5f3b2ed310736c5a266afd869
APanda-DotCom <ashutoshpanda843@gmail.com>
2024-08-26 09:50:34 +0530

    add code

--- src/NetflixSeries.jsx
+++ src/NetflixSeries.jsx
@@ -1,35 +1,19 @@
-import React from 'react';
-import seriseData from "./api/seriseData.json"
+
+import seriseData from "./api/seriseData.json";
+
 
 const NetflixSeries = () => {
- 
   return (
-   <ul>
-    {
-      seriseData.map( (curElem)  =>{
-      return(
-          <li key={curElem.id}>
-      <div>
-        <img 
-        src={curElem.img_url} 
-        alt="" 
-        width="40%"
-        height="40%"/>
-     </div>
-     <h2>Name:{curElem.name} </h2>
-     <h3>Rating:{curElem.rating}</h3>
-     <p>Summary:{curElem.description} </p>
-     <p>Genre:{curElem.genre}</p>
-     <p>Cast:{curElem.cast}</p>
-     <a href={curElem.watch_url} target='blank'>
-     <button> watch Now</button>
-     </a>
-    </li>
-      )
+    <>
+    {seriseData.map ((curElem) =>{
+        <seriseCard key={curElem.id} data ={curElem}/>
       })
     }
-  </ul>
-  )
-}
+  
+    </>
+  );
+};
 
 export default NetflixSeries
+
+
